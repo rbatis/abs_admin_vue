e4fc0a0cef68edec73066402c0e5eaf3c5af96dd
zxj_pc <zhuxiujia@qq.com>
2021-01-31 19:38:20 +0800

    del en

--- src/components/SelectLang/index.jsx
+++ src/components/SelectLang/index.jsx
@@ -4,12 +4,10 @@ import { Icon, Menu, Dropdown } from 'ant-design-vue'
 import { i18nRender } from '@/locales'
 import i18nMixin from '@/store/i18n-mixin'
 
-const locales = ['zh-CN', 'zh-TW', 'en-US', 'pt-BR']
+const locales = ['zh-CN', 'zh-TW']
 const languageLabels = {
   'zh-CN': '简体中文',
-  'zh-TW': '繁体中文',
-  'en-US': 'English',
-  'pt-BR': 'Português'
+  'zh-TW': '繁体中文'
 }
 // eslint-disable-next-line
 const languageIcons = {
