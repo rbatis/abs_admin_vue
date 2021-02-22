0c0e1dbb99a37ec9865db3aca133cc97c9185535
zhuxiujie <zhuxiujia@qq.com>
2021-02-22 12:34:46 +0800

    res data

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,7 +1,7 @@
 export function showMsg(self,res) {
     if (res != null && res.code !== undefined) {
        if (res.code==="SUCCESS"){
-         self.$message.info(res.msg);
+         self.$message.info('成功');
        }else{
          self.$message.error(res.msg);
        }
