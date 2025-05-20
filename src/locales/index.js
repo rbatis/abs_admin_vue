98f906464b88d95ffca989a23ccd3080597229b2
zxj <zhuxiujia@qq.com>
2025-05-20 22:59:24 +0800

    fix

--- src/locales/index.js
+++ src/locales/index.js
@@ -5,14 +5,17 @@ import moment from 'moment'
 
 // default lang
 import enUS from './lang/en-US'
-
+import zhCN from './lang/zh-CN'
 Vue.use(VueI18n)
 
-export const defaultLang = 'en-US'
+export const defaultLang = 'zh-CN'
 
 const messages = {
   'en-US': {
     ...enUS
+  },
+  'zh-CN': {
+    ...zhCN
   }
 }
 
